595a41789570ee8f083a8de1cc1046bbb0728def
jeanagnissan <jean@digital.ci>
2023-01-06 15:40:04 +0000

    update folder Day3/WEEK3_Day3_ExerciceXP

--- script3.js
+++ script3.js
@@ -1,28 +1,30 @@
 
 
 /**
- * Exercice 2
+ * Exercice 3
  */
-console.log("----------------Exercice 2--------------");
+console.log("----------------Exercice 3--------------");
 
 
 // 1- Copiez le code ci-dessus dans un fichier HTML structuré.
 // ok
 
 // 2- Dans votre fichier Javascript, ajoutez la fonctionnalité qui vous permettra de faire glisser le fichier box et de le déposer dans le fichier target. Consultez les notes de cours nommées animations DOM .
-function onDragStart(event) {
-    event.dataTransfer.setData("text/plain", event.target.id);
-    event.dataTransfer.effectAllowed = "move";
-}
- 
-function onDragOver(event) {
-    event.preventDefault();
-    event.dataTransfer.dropEffect = "move"
-}
- 
-function onDrop(event) {
-    event.preventDefault();
-    let data = event.dataTransfer.getData("text/plain");
-    event.target.appendChild(document.getElementById(data));
-}
+let element = document.getElementById("box")
+element.setAttribute('draggable','true');
+
+element.addEventListener("dragstart", function(event) {
+  console.log ("drag " +  "X: " + event.clientX  + " Y: " +  event.clientY);
+});
+
+element.addEventListener("dragend", function(event) {
+    let _x = event.clientX;
+    let _y = event.clientY;
+    console.log(event)
+    event.target.style.left = _x + "px";
+    event.target.style.top = _y + "px";
+    console.log ("dragend" + "X: " + event.clientX  + " Y: " +  event.clientY );
+  });
+
+
 
